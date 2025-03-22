c030cf60a0873edcf2292074ba9f509bf2e114eb
Leonardo Martinelli <leomartinelli07@gmail.com>
2025-03-22 12:07:34 -0300

    Adicionado Projeto Wagner

---
+++ Introdução a Computação Gráfica/1-Bimestre/Apps/Projetos/1BIM/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace ProjetoWagner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtXMin = new System.Windows.Forms.TextBox();
+            this.txtXMax = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(12, 38);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 23);
+            this.txtA.TabIndex = 0;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(12, 83);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 23);
+            this.txtB.TabIndex = 1;
+            // 
+            // txtXMin
+            // 
+            this.txtXMin.Location = new System.Drawing.Point(12, 130);
+            this.txtXMin.Name = "txtXMin";
+            this.txtXMin.Size = new System.Drawing.Size(100, 23);
+            this.txtXMin.TabIndex = 2;
+            // 
+            // txtXMax
+            // 
+            this.txtXMax.Location = new System.Drawing.Point(12, 174);
+            this.txtXMax.Name = "txtXMax";
+            this.txtXMax.Size = new System.Drawing.Size(100, 23);
+            this.txtXMax.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Plotar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "M";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "X Min";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "X Max";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtXMax);
+            this.Controls.Add(this.txtXMin);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtXMin;
+        private System.Windows.Forms.TextBox txtXMax;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Introdução a Computação Gráfica/1-Bimestre/Apps/Projetos/1BIM/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoWagner
+{
+    public partial class Form1 : Form
+    {
+
+        // Variáveis para armazenar os valores da reta
+        private double m = 0;
+        private double b = 0;
+        private double xMin = -10;
+        private double xMax = 10;
+
+        // Lista para armazenar os pontos da reta
+        private List<Tuple<float, float>> pontos = new List<Tuple<float, float>>();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        
+            
+
+        }
+
+       
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Obter o objeto Graphics para desenhar no formulário
+            Graphics g = e.Graphics;
+
+            // Desenhar os eixos x e y
+            Pen caneta = new Pen(Color.Black, 2);
+            int centerX = Width / 2;
+            int centerY = Height / 2;
+
+            // Desenhar o eixo X
+            g.DrawLine(caneta, 0, centerY, Width, centerY);
+
+            // Desenhar o eixo Y
+            g.DrawLine(caneta, centerX, 0, centerX, Height);
+
+            // Desenhar a reta y = mx + b
+            if (pontos.Count > 0)
+            {
+                Pen LinhaCaneta = new Pen(Color.Red, 2);
+                Point[] pontosTela = new Point[pontos.Count];
+
+                // Converter os pontos da lista para coordenadas da tela
+                for (int i = 0; i < pontos.Count; i++)
+                {
+                    float x = pontos[i].Item1; // Coordenada x
+                    float y = pontos[i].Item2; // Coordenada y
+
+                    // Converter coordenadas cartesianas para coordenadas da tela
+                    int screenX = centerX + (int)(x ); // Ajustar escala
+                    int screenY = centerY - (int)(y ); // Inverter Y para o sistema de coordenadas da tela
+
+                    pontosTela[i] = new Point(screenX, screenY);
+                }
+
+                // Desenhar a reta
+                g.DrawLines(LinhaCaneta, pontosTela);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Obter os valores de entrada
+            m = double.Parse(txtA.Text);
+            b = double.Parse(txtB.Text);
+            xMin = double.Parse(txtXMin.Text);
+            xMax = double.Parse(txtXMax.Text);
+
+            // Calcular os pontos da reta e armazená-los na lista
+            pontos.Clear(); // Limpar a lista antes de adicionar novos pontos
+            for (double x = xMin; x <= xMax; x += 0.1) // Incremento de 0.1 para suavizar a reta
+            {
+                double y = m * x + b;
+                pontos.Add(new Tuple<float, float>((float)x, (float)y)); // Armazenar (x, y) como Tuple
+            }
+
+            // Forçar a redesenhar o formulário
+            Invalidate();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        
+    }
+}
+
